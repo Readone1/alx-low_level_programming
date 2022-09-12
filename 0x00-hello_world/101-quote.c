9c00d146f7c09f4721c221e2a3633fe9ed7a4b96
Readone1 <ridwanjackson1@gmail.com>
2022-09-12 01:53:58 -0700

    c language

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,14 +3,10 @@
 #include <unistd.h>
 /**
  * main - main block
- *
  * Return: 1
  */
 int main(void)
-{int s = strlen("and that piece of 
-		art is useful\" - Dora Korpar, 2015-10-19\n");
-write(2,	
-	"and that piece of art					 is useful\" - Dora Korpar, 2015-10-19\n",s);
+{
+	write(1,"and that piece of art is useful\"-Dora Korpar, 2015-10-19\n",59);
 	return (1);
 }
-int
