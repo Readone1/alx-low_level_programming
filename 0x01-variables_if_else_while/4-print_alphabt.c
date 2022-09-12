9b14bb115ac802194aa150a16e5e2cecd1b28db2
Readone1 <ridwanjackson1@gmail.com>
2022-09-12 16:11:04 -0700

    c 2

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - ommit some alphabet
+ * you can only use the putchar
+ * Return: 0
+ */
+
+int main(void)
+{
+	int lw = 'a';
+	while (lw <= 'z')
+	{
+		if (lw == 'e' || lw == 'q')
+		{
+			lw += 1;
+		}
+		else
+		{
+			putchar(lw);
+			lw += 1;
+		}
+	}putchar('\n');
+	return (0);
+}
