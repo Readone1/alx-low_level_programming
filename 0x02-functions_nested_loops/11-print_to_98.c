0aeb81c7bdac80fce0ec1c6a3a449f6200d193f0
Readone1 <ridwanjackson1@gmail.com>
2022-09-14 19:53:45 -0700

    c 3

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,57 +8,30 @@
  * from n to 98, followed by a new line.
  * Return: Nothing
  */
-
 void print_to_98(int n)
-
 {
-
-		if (n <= 98)
-
-				{
-
-							for (; n <= 98; n++)
-
-										{
-
-														printf("%d", n);
-
-
-
-																	if (n == 98)
-
-																						continue;
-
-																				printf(", ");
-
-																						}
-
-									printf("\n");
-
-										}
-
-			else
-
-					{
-
-								for (; n >= 98; n--)
-
-											{
-
-															printf("%d", n);
-
-																		if (n == 98)
-
-																							continue;
-
-																					printf(", ");
-
-																							}
-
-										printf("\n");
-
-											}
-
+if (n <= 98)
+{
+for (; n <= 98; n++)
+{
+printf("%d", n);
+if (n == 98)
+continue;
+printf(", ");
+}
+printf("\n");
+}
+else
+{
+for (; n >= 98; n--)
+{
+printf("%d", n);
+if (n == 98)
+continue;
+printf(", ");
+}
+printf("\n");
+}
 }
 
 
